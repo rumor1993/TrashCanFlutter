701b0409069c8ea2c8ca975d3830305836c807d6
rumor1993 <rumor1993@naver.com>
2021-12-24 02:13:43 +0900

    마커가 하나만 찍히는 이슈가 발생..

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -25,10 +26,9 @@ class _MyAppState extends State<MyApp> {
     OverlayImage.fromAssetImage(
       assetName: 'assets/place-marker.png',
     ).then((image) {
-      setState(() {
         _markers.add(Marker(
             markerId: 'id',
-            position: LatLng(37.581915, 127.0810617),
+            position: LatLng(37.5985707, 127.095467),
             captionText: "커스텀 아이콘",
             captionColor: Colors.indigo,
             captionTextSize: 20.0,
@@ -39,36 +39,50 @@ class _MyAppState extends State<MyApp> {
             width: 45,
             height: 45,
             infoWindow: '인포 윈도우'));
-      });
     });
   }
 
   getData(url) async {
     var response = await http.get( Uri.parse(url) );
     var result = jsonDecode(utf8.decode(response.bodyBytes));
+    var location;
+    var x;
+    var y;
 
     print(result[0]["id"]);
     print(result[0]["region"]);
     print(result[0]["address"]);
     print(result[0]["location"]);
 
-    result.forEach((item) => {
+    result.forEach((item) async => {
+      location = await getGeocode(item['location']),
+      y = double.parse(location[0]),
+      x = double.parse(location[1]),
+
       OverlayImage.fromAssetImage(
        assetName: 'assets/icons8-trash-50.png',
       ).then((image) {
+      setState(() {
+        // var result2 = await getGeocode(result[0]["location"]);
+        print("x: ${x} y: ${y}");
+        // var x = double.parse(result2[0]);
+        // var y = double.parse(result2[1]);
+
         _markers.add(Marker(
-            markerId: 'id',
-            position: LatLng(37.581915, 127.0810617),
-            captionText: "커스텀 아이콘",
-            captionColor: Colors.indigo,
-            captionTextSize: 20.0,
-            icon: image,
-            alpha: 0.8,
-            captionOffset: 30,
-            anchor: AnchorPoint(0.5, 1),
-            width: 20,
-            height: 20,
-            infoWindow: '현위치'));
+        markerId: 'id',
+        position: LatLng(y, x),
+        captionText: "커스텀 아이콘",
+        captionColor: Colors.indigo,
+        captionTextSize: 20.0,
+        icon: image,
+        alpha: 0.8,
+        captionOffset: 30,
+        anchor: AnchorPoint(0.5, 1),
+        width: 30,
+        height: 30,
+        infoWindow: '현위치'));
+        });
+      print("_markers ${_markers.length}");
       })
     });
   }
@@ -77,13 +91,34 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getData("http://172.30.1.43:8080/trash?region=중랑구");
-    addMarker();
+    // addMarker();
+    getData("http://127.0.0.1:8080/trash?region=중랑구");
+
   }
 
-  getLocation() async {
-    // var _locationData = await Location().getLocation();
-    // print(_locationData);
+  getGeocode(address) async {
+    var url =
+    Uri.https('naveropenapi.apigw.ntruss.com', '/map-geocode/v2/geocode',
+        {'query': address});
+
+    Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'X-NCP-APIGW-API-KEY-ID': 'rnbocqpd3n',
+    'X-NCP-APIGW-API-KEY': 'QYEGHpV6Ozckj9LP8yvrS0KDb0WVBMnXvAhuqUu4'
+    };
+
+
+    var response = await http.get(url,
+      headers: headers);
+    if (response.statusCode == 200) {
+      var result = jsonDecode(utf8.decode(response.bodyBytes));
+      // print("responseBody: ${utf8.decode(response.bodyBytes)}");
+
+      print("responseBody ## ${result["addresses"][0]["x"]}");
+      print("responseBody ## ${result["addresses"][0]["y"]}");
+      return [result["addresses"][0]["y"],result["addresses"][0]["x"]];
+    }
   }
 
   @override
@@ -108,6 +143,8 @@ class _MyAppState extends State<MyApp> {
           showUnselectedLabels: false,
           onTap: (tapNumber){
             setState(() {
+              print(_markers.length);
+                _markers = _markers;
             });
           },
           items: [
